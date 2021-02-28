67a95877a0c61128867d18bc35f050e03278ccee
Praveen Mahato <praveenmahato147@gmail.com>
2021-02-28 15:04:36 +0530

    Added UnitOfMeasure.java entity

--- src/main/java/com/example/spring5recipeapp/domain/Ingredient.java
+++ src/main/java/com/example/spring5recipeapp/domain/Ingredient.java
@@ -12,6 +12,8 @@ public class Ingredient {
     private BigDecimal amount;
     @ManyToOne
     private Recipe recipe;
+    @OneToOne(fetch = FetchType.EAGER)
+    private UnitOfMeasure uom;
 
     public Long getId() {
         return id;
@@ -43,5 +45,13 @@ public class Ingredient {
 
     public void setRecipe(Recipe recipe) {
         this.recipe = recipe;
+    }
+
+    public UnitOfMeasure getUom() {
+        return uom;
+    }
+
+    public void setUom(UnitOfMeasure uom) {
+        this.uom = uom;
     }
 }
