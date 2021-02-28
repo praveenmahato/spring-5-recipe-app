e4c3917d54727fa7ec2e72eb32a45836d5fb3bf7
Praveen Mahato <praveenmahato147@gmail.com>
2021-02-28 16:23:53 +0530

    Category entity and created repository

--- src/main/java/com/example/spring5recipeapp/domain/Recipe.java
+++ src/main/java/com/example/spring5recipeapp/domain/Recipe.java
@@ -23,6 +23,11 @@ public class Recipe {
     private Notes notes;
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
     private Set<Ingredient> ingredients;
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty;
+    @ManyToMany
+    @JoinTable(name = "recipe_category",joinColumns = @JoinColumn(name = "recipe_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private Set<Category> categories;
 
     public Long getId() {
         return id;
@@ -111,4 +116,22 @@ public class Recipe {
     public void setIngredients(Set<Ingredient> ingredients) {
         this.ingredients = ingredients;
     }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
+    }
+
+
 }
